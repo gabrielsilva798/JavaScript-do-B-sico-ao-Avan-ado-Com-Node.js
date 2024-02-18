e198a7acc6e1b6c660ccca6b2992f21af6c04167
Gabriel_Silva <gabrielsilva7988@gmail.com>
2024-02-18 20:17:01 -0300

    05. Conhecendo os special numbers

---
+++ secao_2/03-especial_numbers/tipoDeDados.js
@@ -0,0 +1,5 @@
+//             tipos NUMBERS ESPECIAIS (INFINITY,  -INFINITY, NaN)
+
+console.log(typeof Infinity); //para operações que derem infinito
+console.log(typeof -Infinity); //para operações que derem infinito negativas
+console.log(typeof NaN); //quando vc trata algo que não é NUMBER como NUMBER
