7517ac075bbf4159f078fe159092476c76a1c60b
Gabriel_Silva <gabrielsilva7988@gmail.com>
2024-02-17 21:16:22 -0300

    02. O que são tipos de dados

---
+++ secao_2/tipos_dados_number/tipoDeDados.js
@@ -0,0 +1,10 @@
+//             TYPEOF(argumento) para ver o tipo de dado
+const number = -4;
+console.log("timpo numérico: ", typeof(number))
+
+//tanto numérico quanto float/decimal são tipos NUMBER.
+const decimal = 4.5;
+console.log("timpo Decimal/Float: ", typeof(decimal))
+
+const tipoString = "Gabeiel";
+console.log("timpo String: ", typeof(tipoString))
