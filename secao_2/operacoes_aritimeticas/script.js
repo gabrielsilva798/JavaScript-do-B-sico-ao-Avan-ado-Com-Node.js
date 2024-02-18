536dc22266809d01f9e9a3ab925ad7aaf9fba91a
Gabriel_Silva <gabrielsilva7988@gmail.com>
2024-02-17 21:37:21 -0300

    04. Operações aritméticas com number

---
+++ secao_2/operacoes_aritimeticas/script.js
@@ -0,0 +1,14 @@
+/*           OPERADORES ARITIMÉTICOS
+            não vou anotar pq é a mesma lôgica do PYTHON e do JAVA.                      */
+
+console.log(2 + 3);
+console.log(5 - 12);
+console.log(4 * 4);
+console.log(5 / 3); //o resultdo é com casas decimais e mesmo assim ainda é um NUMBER.
+console.log(20 % 2);
+console.log(21 % 2);
+console.log(2 * (3 - 2));
+
+//TODOS OS RESULTADOS SÃO NUMBERS   
+console.log(typeof(5 / 30));
+console.log(typeof(20 % 2));
